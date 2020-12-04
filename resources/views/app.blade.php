93e276b405c1003645f6ae3d0229248efbca0b60
Shiraishi Atsuya <i68s2j8rxy@gmail.com>
2020-12-04 18:06:18 +0900

    6th commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,9 +12,10 @@
         <div id="nav">
           <router-link to="/">Home</router-link>
           <router-link to="/about">About</router-link>
+          <router-link to="/test">Test</router-link>
+          <router-link to="/list">List</router-link>
         </div>
         <router-view/>
-        </div>
     </div>
 
 <script src="{{ mix('js/app.js') }}"></script> 
