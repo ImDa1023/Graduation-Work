33eb6befa5229a88f12a674a83de529d33d6e9ee
Shiraishi Atsuya <i68s2j8rxy@gmail.com>
2020-12-15 01:33:17 +0900

    13th commit

---
+++ resources/js/components/Manu.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <h1>Manu</h1>
+    <div id="nav">
+      <router-link to="/setting">管理</router-link>
+      <router-link to="/list">楽譜リスト</router-link>
+      <router-link to="/notice">お知らせ</router-link>
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  name: "manu",
+};
+</script>
