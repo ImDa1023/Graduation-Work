f0702d1d88cc28c5c0d7bc3e6e51ef89609a3d9f
Shiraishi Atsuya <i68s2j8rxy@gmail.com>
2020-12-18 19:28:51 +0900

    33rd commit

--- public/js/app.js
+++ public/js/app.js
@@ -39237,7 +39237,7 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", [_c("h2", [_vm._v("お知らせ")])])
+    return _c("div", [_c("h2", [_vm._v("お知らせ画面")])])
   }
 ]
 render._withStripped = true
