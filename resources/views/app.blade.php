e93decc6a3e1786d41f90b872695625f8a402c7d
Shiraishi Atsuya <i68s2j8rxy@gmail.com>
2020-12-24 17:45:13 +0900

    39th commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name=”viewport” content=”width=device-width,initial-scale=1.0″>
     <title>{{ config('app.name') }}</title>
     
 <script src="{{ mix('js/app.js') }}" defer></script>
