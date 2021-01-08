9a0d2028fb049535f1db56fe1bf321d03494dc64
kottoncandy <k.candy@gmail.com>
2021-01-08 13:59:51 +0900

    64th commit

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <header v-if="$route.name !== 'タイトル'">
-      <Header></Header>
+      <Header />
     </header>
 
     <main>
@@ -9,13 +9,23 @@
     </main>
 
     <footer v-if="$route.name !== 'タイトル' && $route.name !== '初期設定'">
-      <Footer-Menu></Footer-Menu>
+      <Footer-Menu />
     </footer>
   </div>
 </template>
 <style>
+html {
+  height: 100%;
+}
+
 body {
+  height: 100%;
   background-color: #f6f6f6;
+  margin: 0;
+}
+main {
+  height: 100%;
+  overflow: hidden;
 }
 h1 {
   font-size: 1rem;
@@ -27,11 +37,36 @@ h2 {
   font-family: "Meylio UI";
   font-weight: 550;
   letter-spacing: 0.15em;
+  margin: 0px;
 }
 a {
   font-size: 1rem;
 }
 li {
   list-style-type: none;
+}
+
+@media screen and (min-width: 1024px) {
+  #app {
+    display: grid;
+    width: 100%;
+    grid-template-areas:
+      "hed hed hed"
+      "fot wap wap";
+    grid-template-rows: 55px 1fr;
+    grid-template-columns: 80px 1fr;
+  }
+
+  #app > header {
+    grid-area: hed;
+  }
+
+  #app > main {
+    grid-area: wap;
+  }
+
+  #app > footer {
+    grid-area: fot;
+  }
 }
 </style>
